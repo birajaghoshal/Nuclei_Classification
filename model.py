26e4c6fa8303b08ab0a4c8238eb556307de42bce
Jacob Carse <jcarse@dundee.ac.uk>
2018-04-18 14:21:37 +0100

    Created copy constructor

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 class Model:
     def __init__(self, config):
-        """ Initialiser for the Model class
+        """ Initialiser for the Model class.
         :param config: A ArgumentParser object
         """
 
@@ -21,3 +21,11 @@ class Model:
         self.Y = tf.placeholder("float", [None, self.num_classes], name='Y')
 
         self.model = "model"
+
+    def __copy__(self):
+        """ Resets the model and returns a copy of the model.
+        :return: A reset copy of the reset Model
+        """
+
+        tf.reset_default_graph()
+        return Model(self.config)
