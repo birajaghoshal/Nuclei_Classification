0f0fe406f57852f73d1c6da1adf8990c92fd6d20
Jacob Carse <jcarse@dundee.ac.uk>
2018-04-18 14:00:05 +0100

    Logging function

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import config
 
-if __name__ == '__main__':
+
+def log(config, message):
+    """ Function to handle printing and logging of messages.
+    :param config: An ArgumentParser object.
+    :param message: String of message to be printed and logged.
+    """
+    if config.verbose:
+        print(message)
+    if config.log_file != '':
+        print(message, file=open(config.log_file, 'a'))
+
+
+if __name__ == "__main__":
     config = config.load_configs()
-    pass
