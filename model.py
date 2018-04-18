c7bad8b5991637490a04485ea27c4a40fac7a36d
Jacob Carse <jcarse@dundee.ac.uk>
2018-04-18 14:46:53 +0100

    Added fully connected function

--- model.py
+++ model.py
@@ -49,7 +49,7 @@ class Model:
         """
 
         def max_pool(input_tensor, k, d, name):
-            """ Creates a Max Pooling layer for the model
+            """ Creates a Max Pooling layer operation.
             :param input_tensor: The tensor for the max pooling layer to use as input.
             :param k: The size of the filters.
             :param d: The size of the strides.
@@ -60,7 +60,7 @@ class Model:
             return tf.nn.max_pool(input_tensor, ksize=[1, k, k, 1], strides=[1, d, d, 1], padding='VALID', name=name)
 
         def convolution(input_tensor, k, d, n_out, name, activation_fn=tf.nn.relu, batch_norm=True):
-            """ Creates a Convolutional layer.
+            """ Creates a Convolutional layer operation.
             :param input_tensor: The tensor for the Convolutional layer will use as input.
             :param k: The size of the filters.
             :param d: The size of the strides.
@@ -71,7 +71,7 @@ class Model:
             :return: A tensor.
             """
 
-            # Gets the number of filters from the input tensor.
+            # Get the number of filters from the input tensor.
             n_in = input_tensor.get_shape()[-1].value
 
             with tf.variable_scope(name):
@@ -80,7 +80,7 @@ class Model:
                 biases = tf.get_variable("bias", [n_out], tf.float32, tf.constant_initializer(0.0))
 
                 # Creates the convolutional tensor operations with the declared weights and biases.
-                conv = tf.nn.conv2d(input_tensor, weights, (1, d, d, 1), padding='VALID')
+                conv = tf.nn.conv2d(input_tensor, weights, (1, d, d, 1), padding="VALID")
                 logits = tf.nn.bias_add(conv, biases)
 
                 # Creates the operation for the activation applied to the logits.
@@ -91,3 +91,34 @@ class Model:
                     return layers.batch_norm(activations)
                 else:
                     return activations
+
+        def fully_connected(input_tensor, n_out, name, activation_fn=tf.nn.relu, batch_norm=True):
+            """ Creates a fully connected layer operation.
+            :param input_tensor: The tensor the fully connected layer use take as input.
+            :param n_out: The number of output filters.
+            :separam name: THe name of the operation.
+            :param activation_fn: The activation function to be applied.
+            :param batch_norm:If batch normalisation should be applied.
+            :return: A tensor.
+            """
+
+            # Get the number of filters from the input tensor.
+            n_in = input_tensor.get_shape()[-1].value
+
+            with tf.variable_scope(name):
+                # Declares the weights and biases for the fully connected layer.
+                weights = tf.get_variable("weights", [n_in, n_out], tf.float32, layers.xavier_initializer())
+                biases = tf.get_variable("bias", [n_out], tf.float32, tf.constant_initializer(0.0))
+
+                # Creates the fully connected operation with the declared weights and biases.
+                logits = tf.nn.bias_add(tf.matmul(input_tensor, weights), biases)
+
+                # Creates the operation for the activation applied to the logits.
+                activations = activation_fn(logits)
+
+                # Creates the batch normalisation operation if required.
+                if batch_norm:
+                    return layers.batch_norm(activations)
+                else:
+                    return activations
+
