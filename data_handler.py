5fd841e91df9acd0a6fb4bfb18cb568ab090a021
Jacob Carse <jacob.carse@gmail.com>
2018-05-02 14:45:38 +0100

    Prediction Dataset Loading

--- data_handler.py
+++ data_handler.py
@@ -87,10 +87,10 @@ class DataHandler:
         val_y = np.array([i for j, i in enumerate(self.data_y) if j in indices])
         self.data_x = np.delete(self.data_x, indices)
         self.data_y = np.delete(self.data_y, indices)
-        if self.config.combine.lower() == 'add':
+        if self.config.combine.lower() == "add":
             self.val_x = np.append(self.val_x, val_x)
             self.val_y = np.append(self.val_y, val_y)
-        elif self.config.combine.lower() == 'replace':
+        elif self.config.combine.lower() == "replace":
             self.val_x = val_x
             self.val_y = val_y
 
@@ -113,19 +113,18 @@ class DataHandler:
 
         # Logs the number of patches.
         self.class_weights = list(Counter(self.train_y).values())
-        self.log('Training Patches: ' + str(len(self.train_y)))
-        self.log('Validation Patches: ' + str(len(self.val_y)))
+        self.log("Training Patches: " + str(len(self.train_y)))
+        self.log("Validation Patches: " + str(len(self.val_y)))
 
     def set_training_data(self, indices):
         """ Sets data from the unlabelled data to the training set.
         :param indices: A list of indices to be moved from unlabelled to training.
         """
 
+        # Sets the full list of indices
         full_indices = []
         for index in indices:
             full_indices.append(list(range(index, index + self.config.cell_patches)))
-
-
 
         # Sets temparary lists to the data to be added.
         temp_x = np.array([i for j, i in enumerate(self.data_x) if j in full_indices])
@@ -140,10 +139,10 @@ class DataHandler:
             temp_x, temp_y = self.balance(temp_x, temp_y)
 
         # Adds the data depending on specified method.
-        if self.config.combine.lower() == 'add':
+        if self.config.combine.lower() == "add":
             self.train_x = np.append(self.train_x, temp_x)
             self.train_y = np.append(self.train_y, temp_y)
-        elif self.config.combine.lower() == 'replace':
+        elif self.config.combine.lower() == "replace":
             self.train_x = temp_x
             self.train_y = temp_y
 
@@ -152,8 +151,8 @@ class DataHandler:
 
         # Logs the number of patches.
         self.class_weights = list(Counter(self.train_y).values())
-        self.log('Training Patches: ' + str(len(self.train_y)))
-        self.log('Validation Patches: ' + str(len(self.val_y)))
+        self.log("Training Patches: " + str(len(self.train_y)))
+        self.log("Validation Patches: " + str(len(self.val_y)))
 
     def get_num_batches(self, train_batch_size, test_batch_size):
         """ The the number of the batches for each set of data.
@@ -182,7 +181,7 @@ class DataHandler:
         image = tf.image.decode_image(image_file, channels=self.config.input_channels)
 
         # Returens the image and label.
-        return tf.cast(image, 'float'), one_hot_label
+        return tf.cast(image, "float"), one_hot_label
 
     def get_datasets(self, train_batch_size, test_batch_size):
         """ Produces the dataset objects to be used by the model.
@@ -220,3 +219,33 @@ class DataHandler:
 
         # Returns the dataset objects.
         return train_dataset, test_dataset, val_dataset
+
+    def get_num_prediction_batches(self, batch_size):
+        """ The the number of the batches for the unlabelled set of data.
+        :param batch_size: The size of the prediction batches.
+        :return: The number of batches for the predictions sets.
+        """
+
+        return int(np.ceil(len(self.data_y) / batch_size))
+
+    def predict_input_parser(self, image_path):
+        """ Operation for the parser to dynamically load images in the dataset for prediction.
+        :param image_path: THe file path to the image.
+        :return: An operation to load the image and create a one hot label.
+        """
+
+        image_file = tf.read_file(image_path)
+        image = tf.image.decode_image(image_file, channels=self.config.input_channels)
+
+        return tf.cast(image, "float")
+
+    def get_prediction_datasets(self, batch_size):
+        """ Produces the dataset objects to be used by the model for predictions.
+        :param batch_size: The size of the batches for predictions.
+        :return: The datasets objects to dynamically load the data for prediction.
+        """
+
+        images = tf.constant(np.asarray(self.data_x))
+        predict_dataset = tf.data.Dataset.from_tensor_slices((images))
+        predict_dataset = predict_dataset.map(self.predict_input_parser, num_parallel_calls=1000).prefetch(1000)
+        return predict_dataset.batch(batch_size)
