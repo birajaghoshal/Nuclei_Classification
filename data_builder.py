2547f2eafbf900701612708dd7919483430937b8
Jacob Carse <jacob.carse@gmail.com>
2018-05-05 10:23:51 +0100

    Fixed Data Building

--- data_builder.py
+++ data_builder.py
@@ -6,10 +6,10 @@ import scipy.io as io
 from collections import Counter
 
 
-def create_training_data(in_dir, out_dir, patch_size, stride):
+def data_builder(in_dir, out_dir, patch_size, stride):
     patch_count, cell_count = 0, 0
-    b_size = patch_size // 2
     start = int(sorted(os.listdir(in_dir))[0].replace("img", ""))
+    b = patch_size // 2
 
     patch_files, classifications = [], []
     if not os.path.isdir(out_dir):
@@ -17,8 +17,7 @@ def create_training_data(in_dir, out_dir, patch_size, stride):
 
     for img_num in range(start, start + len(os.listdir(in_dir))):
         image = cv2.imread(in_dir + "img" + str(img_num) + "/img" + str(img_num) + ".bmp")
-        border_image = cv2.copyMakeBorder(image, b_size, b_size, b_size, b_size, cv2.BORDER_WRAP)
-
+        border_image = cv2.copyMakeBorder(image, patch_size, patch_size, patch_size, patch_size, cv2.BORDER_WRAP)
         epi = io.loadmat(in_dir + "img" + str(img_num) + "/img" + str(img_num) + "_epithelial.mat")["detection"]
         fib = io.loadmat(in_dir + "img" + str(img_num) + "/img" + str(img_num) + "_fibroblast.mat")["detection"]
         inf = io.loadmat(in_dir + "img" + str(img_num) + "/img" + str(img_num) + "_inflammatory.mat")["detection"]
@@ -26,22 +25,25 @@ def create_training_data(in_dir, out_dir, patch_size, stride):
         point_sets = [epi, fib, inf, other]
 
         for p in range(len(point_sets)):
-            for point in point_sets[p]:
+            for point in np.round(point_sets[p]).astype(int):
                 patches = []
-                for i in range(int(point[0]) - 1, int(point[0]) + 6, stride):
-                    for j in range(int(point[1]) - 3, int(point[1]) + 4, stride):
-                        patches.append(border_image[j-4:j + patch_size -3, i-4:i + patch_size -3])
+                for i in range(round(point[0]) - 3 + patch_size, round(point[0]) + 3 + patch_size, stride):
+                    for j in range(round(point[1]) - 3 + patch_size, round(point[1]) + 3 + patch_size, stride):
+                        patch = border_image[j - b: j + b + 1, i - b: i + b + 1]
+                        patches.append(patch)
                         for x in [0, 1, 2]:
-                            patches.append(cv2.flip(patches[0], x))
+                            patch = cv2.flip(patches[0], x)
+                            patches.append(patch)
                             for y in [1, 2, 3]:
-                                patches.append(np.rot90(patches[0], y))
+                                temp_patch = np.rot90(patch, y)
+                                patches.append(temp_patch)
                         for m in range(len(patches)):
                             cv2.imwrite(out_dir + '/' + str(patch_count) + ".bmp", patches[m])
-                            if patch_count % 100 == 0:
+                            patch_count += 1
+                            if patch_count % 1000 == 0:
                                 print(str(patch_count) + " Completed!")
                             patch_files.append(str(patch_count) + ".bmp")
                             classifications.append(p)
-                            patch_count += 1
                             cell_count += 1
                         patches = []
                 print("Patches per cell: " + str(cell_count))
@@ -62,4 +64,4 @@ if __name__ == "__main__":
     if out_dir[-1] != '/':
         out_dir += '/'
 
-    create_training_data(in_dir, out_dir, int(sys.argv[3]), int(sys.argv[4]))
+    data_builder(in_dir, out_dir, int(sys.argv[3]), int(sys.argv[4]))
